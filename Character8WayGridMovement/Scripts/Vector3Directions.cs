db325d11d7b75d59463ccb2ea3b6d7b07f53c1a6
Phlux Apps <jc@phluxapps.com>
2020-03-19 12:46:00 -0500

    add 8 way grid movement with examples

---
+++ Character8WayGridMovement/Scripts/Vector3Directions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class Vector3Directions
+{
+    public static Vector3 UpLeft { get { return new Vector3(-1, 1, 0); } }
+    public static Vector3 UpRight { get { return new Vector3(1, 1, 0); } }
+    public static Vector3 DownLeft { get { return new Vector3(-1, -1, 0); } }
+    public static Vector3 DownRight { get { return new Vector3(1, -1, 0); } }
+    public static Vector3 ForwardLeft { get { return new Vector3(-1, 0, 1); } }
+    public static Vector3 ForwardRight { get { return new Vector3(1, 0, 1); } }
+    public static Vector3 BackLeft { get { return new Vector3(-1, 0, -1); } }
+    public static Vector3 BackRight { get { return new Vector3(1, 0, -1); } }
+}
