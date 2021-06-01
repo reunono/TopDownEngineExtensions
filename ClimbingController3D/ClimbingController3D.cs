d0ece8e2daea788870e4e4d5c06414d32c216695
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-06-01 11:09:22 +0300

    ✨ Adds ClimbingController3D by AlexanderGheorghe

---
+++ ClimbingController3D/ClimbingController3D.cs
@@ -0,0 +1,22 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace ClimbingControllerExtension
+{
+    /// <summary>
+    /// An extension of TopDownController3D which gives you the option (activated by default) to have CharacterDash3D climb slopes instead of stopping at obstacles
+    /// </summary>
+    [AddComponentMenu("TopDown Engine/Character/Core/Climbing Controller 3D")]
+    public class ClimbingController3D : TopDownController3D
+    {
+        [Tooltip("if this option is checked, the CharacterDash3D will climb slopes, else the behaviour will be the same as with TopDownController3D")]
+        public bool ClimbWhileDashing = true;
+        public override void MovePosition(Vector3 newPosition)
+        {
+            if (ClimbWhileDashing)
+                _characterController.Move(newPosition - _transform.position);
+            else
+                base.MovePosition(newPosition);
+        }
+    }
+}
