5cdd3655153dc5f928add46232ad6bf2645c7e80
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-07-03 16:07:30 +0300

    ✨ Adds TypedDamage

---
+++ TypedDamage/TypedHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace TopDownEngineExtensions
+{
+    public class TypedHealth : Health
+    {
+        [Header("Damage types")]
+        public float VulnerableDamageTypeMultiplier = 2;
+        public DamageType[] VulnerableDamageTypes;
+        public DamageType[] InvulnerableDamageTypes;
+        
+        public void Damage(int damage, GameObject instigator, float flickerDuration, float invincibilityDuration, Vector3 damageDirection, DamageType damageType)
+        {
+            if (damageType != null)
+            {
+                if (InvulnerableDamageTypes.Any(invulnerableDamageType => damageType == invulnerableDamageType)) return;
+                if (VulnerableDamageTypes.Any(vulnerableDamageType => damageType == vulnerableDamageType))
+                    damage = (int) (damage * VulnerableDamageTypeMultiplier);
+            }
+            base.Damage(damage, instigator, flickerDuration, invincibilityDuration, damageDirection);
+        }
+    }
+}
