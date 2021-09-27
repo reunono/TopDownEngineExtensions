3fe9b496111e9190afae354a4a57c13d49aba059
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-09-27 23:34:34 +0300

    ✨ Adds TriggerEventOnAreaClear

---
+++ TriggerEventOnAreaClear/TriggerEventOnAreaClear.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace TopDownEngineExtensions
+{
+    /// <summary>
+    /// Requires a Collider and objects with the AreaClearTarget component inside said collider
+    /// </summary>
+    public class TriggerEventOnAreaClear : MonoBehaviour
+    {
+        /// the event to trigger when the area is cleared (all objects marked with AreaClearTarget have been destroyed)
+        [Tooltip("the event to trigger when the room is cleared (all enemies are dead)")]
+        public UnityEvent OnRoomCleared;
+        private int _numberOfEnemiesAlive;
+
+        public void AddEnemy() { _numberOfEnemiesAlive++; }
+
+        public void RemoveEnemy()
+        {
+            _numberOfEnemiesAlive--;
+            if (_numberOfEnemiesAlive == 0) OnRoomCleared?.Invoke();
+        }
+    }
+}
