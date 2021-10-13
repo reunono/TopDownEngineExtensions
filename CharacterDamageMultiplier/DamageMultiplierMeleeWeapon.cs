909fed2dd6ccaadaa88e300a728984a117fa3e5f
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2021-10-13 09:57:19 +0300

    ✨ Adds CharacterDamageMultiplier

---
+++ CharacterDamageMultiplier/DamageMultiplierMeleeWeapon.cs
@@ -0,0 +1,9 @@
+﻿using MoreMountains.TopDownEngine;
+
+namespace TopDownEngineExtensions
+{
+    public class DamageMultiplierMeleeWeapon : MeleeWeapon
+    {
+        public override void ApplyDamageMultiplier(float multiplier) { _damageOnTouch.DamageCaused = (int)(DamageCaused * multiplier); }
+    }
+}
---
+++ CharacterDamageMultiplier/PickableDamageMultiplier.cs
@@ -0,0 +1,23 @@
+﻿using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace TopDownEngineExtensions
+{
+    public class PickableDamageMultiplier : PickableItem
+    {
+        public float DamageMultiplier = 2f;
+        private DamageMultiplierCharacterHandleWeapon _handleWeapon;
+
+        protected override void Pick(GameObject picker)
+        {
+            _handleWeapon.DamageMultiplier = DamageMultiplier;
+        }
+
+        protected override bool CheckIfPickable()
+        {
+            _handleWeapon = _collidingObject.MMGetComponentNoAlloc<Character>()?.FindAbility<DamageMultiplierCharacterHandleWeapon>();
+            return _handleWeapon != null;
+        }
+    }
+}
