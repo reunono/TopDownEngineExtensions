bc7c473f2fc1035c4e528051b49114d519a35068
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-02-09 10:42:47 +0200

    ✨ Adds ProgressionSystem

---
+++ ProgressionSystem/Scripts/ExperienceGivers/GiveExperienceToPlayerOnDeath.cs
@@ -0,0 +1,15 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace ProgressionSystem
+{
+    public class GiveExperienceToPlayerOnDeath : MonoBehaviour
+    {
+        public int Experience = 50;
+        private void Start()
+        {
+            var health = GetComponent<Health>();
+            if (health != null) health.OnDeath += () => ExperienceEvent.Trigger(ExperienceEventType.Add, LevelManager.Instance.Players[0].gameObject, Experience);
+        }
+    }
+}
---
+++ ProgressionSystem/Scripts/LevelUpActions/PlayFeedbacksOnLevelUp.cs
@@ -0,0 +1,13 @@
+﻿using MoreMountains.Feedbacks;
+
+namespace ProgressionSystem
+{
+    public class PlayFeedbacksOnLevelUp : DoSomethingOnLevelUp
+    {
+        public MMFeedbacks LevelUpFeedbacks;
+        protected override void OnLevelUp(int level, int maxLevel)
+        {
+            LevelUpFeedbacks?.PlayFeedbacks(transform.position, level);
+        }
+    }
+}
