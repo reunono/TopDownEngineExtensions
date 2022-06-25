c68684927b966229f1c0ecc73a9a003067e6ec89
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-06-25 20:42:47 +0300

    Adds Vehicle

---
+++ Vehicle/Scripts/Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Vehicle : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Suspension[] _suspensions;
+    private VehicleController _vehicleController;
+    private int _initialLayer;
+    private GameObject _player;
+    public UnityEvent OnEnableEvent;
+    public UnityEvent OnDisableEvent;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _suspensions = GetComponentsInChildren<Suspension>(true);
+        _vehicleController = GetComponent<VehicleController>();
+        _initialLayer = gameObject.layer;
+    }
+
+    private void OnEnable()
+    {
+        OnEnableEvent.Invoke();
+        _player = LevelManager.Instance.Players[0].gameObject;
+        _player.SetActive(false);
+        _player.transform.parent = transform;
+        gameObject.layer = _player.layer;
+        _rigidbody.isKinematic = false;
+        foreach (var suspension in _suspensions)
+            suspension.gameObject.SetActive(true);
+        _vehicleController.enabled = true;
+    }
+    
+    private void OnDisable()
+    {
+        OnDisableEvent.Invoke();
+        gameObject.layer = _initialLayer;
+        _rigidbody.isKinematic = true;
+        foreach (var suspension in _suspensions)
+            suspension.gameObject.SetActive(false);
+        _vehicleController.enabled = false;
+        _player.transform.parent = null;
+        _player.transform.rotation = Quaternion.identity;
+        _player.SetActive(true);
+    }
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Player1_Interact")) enabled = false;
+    }
+}
---
+++ Vehicle/ThirdParty/Vehicle-Physics/Scripts/Suspension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class Suspension : MonoBehaviour 
+{
+	private Rigidbody _rigidbody;
+	[SerializeField] private float WheelRadius = 0.5f;
+	[SerializeField] private float SuspensionDistanceConstant = 1f;
+	[SerializeField] private float SpringConstant = 30000f;
+	[SerializeField] private float DamperConstant = 4000f;
+
+	private float _previousSuspensionDistance;
+	private float _currentSuspensionDistance;
+	private float _springVelocity;
+	private float _springForce;
+	private float _damperForce;
+
+	private void Awake()
+	{
+		_rigidbody = GetComponentInParent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		// Vehicle uses a raycast suspension system
+		var ray = new Ray(transform.position, -transform.up);
+		if (!Physics.Raycast(ray, out var hit, SuspensionDistanceConstant + WheelRadius)) return;
+		// Hooke's Law
+		_previousSuspensionDistance = _currentSuspensionDistance;
+		_currentSuspensionDistance = SuspensionDistanceConstant - (hit.distance - WheelRadius);
+		_springVelocity = (_currentSuspensionDistance - _previousSuspensionDistance) / Time.fixedDeltaTime;
+		_springForce = SpringConstant * _currentSuspensionDistance;
+		_damperForce = DamperConstant * _springVelocity;
+
+		// Apply force to car body
+		_rigidbody.AddForceAtPosition(transform.up * (_springForce + _damperForce), transform.position, ForceMode.Force);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Debug.DrawRay(transform.position, -transform.up * SuspensionDistanceConstant, Color.blue, 0f);
+	}
+}
