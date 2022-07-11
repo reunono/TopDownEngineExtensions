b610366ec665cf6212f76628e59cdde22d37c0a3
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-07-11 07:22:21 +0300

    ✨ Adds GridPathfinding

---
+++ GridPathfinding/GridCharacterPathfindToMouse3D.cs
@@ -0,0 +1,19 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+public class GridCharacterPathfindToMouse3D : CharacterPathfindToMouse3D
+{
+    protected override void Initialization()
+    {
+        _abilityInitialized = true;
+        _mainCamera = Camera.main;
+        _characterPathfinder3D = gameObject.GetComponent<CharacterPathfinder3D>();
+        _character.FindAbility<CharacterGridMovement>().InputMode = CharacterGridMovement.InputModes.Script;
+            
+        OnClickFeedbacks?.Initialization();
+        _playerPlane = new Plane(Vector3.up, Vector3.zero);
+        if (Destination != null) return;
+        Destination = new GameObject();
+        Destination.name = this.name + "PathfindToMouseDestination";
+    }
+}
