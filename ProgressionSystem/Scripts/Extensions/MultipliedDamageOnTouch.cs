b6a0cd8ca9db8baf79acfcd86a1b5665bfd8d966
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-07-16 06:04:56 +0300

    Updated Progression to work with latest TDE

--- ProgressionSystem/Scripts/Extensions/MultipliedDamageOnTouch.cs
+++ ProgressionSystem/Scripts/Extensions/MultipliedDamageOnTouch.cs
@@ -8,78 +8,35 @@ namespace ProgressionSystem.Scripts.Extensions
     public class MultipliedDamageOnTouch : DamageOnTouch
     {
         [SerializeField] private FloatVariable DamageMultiplier;
-        
+
         protected override void OnCollideWithDamageable(Health health)
         {
+            if (!health.CanTakeDamageThisFrame())
+            {
+                return;
+            }
+			
             // if what we're colliding with is a TopDownController, we apply a knockback force
             _colliderTopDownController = health.gameObject.MMGetComponentNoAlloc<TopDownController>();
-            _colliderRigidBody = health.gameObject.MMGetComponentNoAlloc<Rigidbody>();
 
-            if ((_colliderTopDownController != null) && (DamageCausedKnockbackForce != Vector3.zero) && (!_colliderHealth.Invulnerable) && (!_colliderHealth.ImmuneToKnockback))
+            HitDamageableFeedback?.PlayFeedbacks(transform.position);
+
+            // we apply the damage to the thing we've collided with
+            var randomDamage = DamageMultiplier.Value * Random.Range(MinDamageCaused, Mathf.Max(MaxDamageCaused, MinDamageCaused));
+			
+            ApplyKnockback(randomDamage, TypedDamages);
+
+            if (RepeatDamageOverTime)
             {
-                _knockbackForce = DamageCausedKnockbackForce;
-
-                if (_twoD) // if we're in 2D
-                {
-                    switch (DamageCausedKnockbackDirection)
-                    {
-                        case KnockbackDirections.BasedOnSpeed:
-                            Vector3 totalVelocity = _colliderTopDownController.Speed + _velocity;
-                            _knockbackForce = Vector3.RotateTowards(DamageCausedKnockbackForce, totalVelocity.normalized, 10f, 0f);
-                            break;
-                        case KnockbackDirections.BasedOnOwnerPosition:
-                            if (Owner == null) { Owner = this.gameObject; }
-                            Vector3 relativePosition = _colliderTopDownController.transform.position - Owner.transform.position;
-                            _knockbackForce = Vector3.RotateTowards(DamageCausedKnockbackForce, relativePosition.normalized, 10f, 0f);
-                            break;
-                        case KnockbackDirections.BasedOnDirection:
-                            Vector3 direction = this.transform.position - _positionLastFrame;
-                            _knockbackForce = direction * DamageCausedKnockbackForce.magnitude;
-                            break;
-                        case KnockbackDirections.BasedOnScriptDirection:
-                            _knockbackForce = _scriptDirection * DamageCausedKnockbackForce.magnitude;
-                            break;
-                    }
-                }
-                else // if we're in 3D
-                {
-                    switch (DamageCausedKnockbackDirection)
-                    {
-                        case KnockbackDirections.BasedOnSpeed:
-                            Vector3 totalVelocity = _colliderTopDownController.Speed + _velocity;
-                            _knockbackForce = DamageCausedKnockbackForce * totalVelocity.magnitude;
-                            break;
-                        case KnockbackDirections.BasedOnOwnerPosition:
-                            if (Owner == null) { Owner = this.gameObject; }
-                            Vector3 relativePosition = _colliderTopDownController.transform.position - Owner.transform.position;
-                            _knockbackForce.x = relativePosition.normalized.x * DamageCausedKnockbackForce.x;
-                            _knockbackForce.y = DamageCausedKnockbackForce.y;
-                            _knockbackForce.z = relativePosition.normalized.z * DamageCausedKnockbackForce.z;
-                            break;
-                        case KnockbackDirections.BasedOnDirection:
-                            Vector3 direction = this.transform.position - _positionLastFrame;
-                            _knockbackForce = direction * DamageCausedKnockbackForce.magnitude;
-                            break;
-                        case KnockbackDirections.BasedOnScriptDirection:
-                            _knockbackForce = _scriptDirection * DamageCausedKnockbackForce.magnitude;
-                            break;
-                    }
-                }
-
-                if (DamageCausedKnockbackType == KnockbackStyles.AddForce)
-                {
-                    _colliderTopDownController.Impact(_knockbackForce.normalized, _knockbackForce.magnitude);
-                }
+                _colliderHealth.DamageOverTime(randomDamage, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection, TypedDamages, AmountOfRepeats, DurationBetweenRepeats, DamageOverTimeInterruptible, RepeatedDamageType);	
+            }
+            else
+            {
+                _colliderHealth.Damage(randomDamage, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection, TypedDamages);	
             }
 
-            HitDamageableFeedback?.PlayFeedbacks(this.transform.position);
-            
-            // we apply the damage to the thing we've collided with
-            
-            int randomDamage = UnityEngine.Random.Range(MinDamageCaused, Mathf.Max(MaxDamageCaused, MinDamageCaused));
-            randomDamage = (int)(randomDamage * DamageMultiplier.Value);
-            _colliderHealth.Damage(randomDamage, gameObject, InvincibilityDuration, InvincibilityDuration, _damageDirection);
-            if ((DamageTakenEveryTime + DamageTakenDamageable > 0) && !_colliderHealth.PreventTakeSelfDamage)
+            // we apply self damage
+            if (DamageTakenEveryTime + DamageTakenDamageable > 0 && !_colliderHealth.PreventTakeSelfDamage)
             {
                 SelfDamage(DamageTakenEveryTime + DamageTakenDamageable);
             }
