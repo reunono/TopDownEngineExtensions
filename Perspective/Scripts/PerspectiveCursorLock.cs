e5072d60dc8e593ed74491d7ce87d170582c2398
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-08-11 13:36:38 +0300

    Changes FirstPerson to Perspective

---
+++ Perspective/Scripts/PerspectiveCursorLock.cs
@@ -0,0 +1,20 @@
+﻿using MoreMountains.Tools;
+using UnityEngine;
+
+public class PerspectiveCursorLock : MonoBehaviour, MMEventListener<PerspectiveChangeEvent>
+{
+    private void Awake()
+    {
+        this.MMEventStartListening();
+    }
+
+    private void OnDestroy()
+    {
+        this.MMEventStopListening();
+    }
+
+    public void OnMMEvent(PerspectiveChangeEvent perspectiveChangeEvent)
+    {
+        Cursor.lockState = perspectiveChangeEvent.NewPerspective == Perspectives.FirstPerson ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+}
---
+++ Perspective/Scripts/PerspectiveModel.cs
@@ -0,0 +1,47 @@
+﻿using MoreMountains.Tools;
+using UnityEngine;
+
+public class PerspectiveModel : MonoBehaviour, MMEventListener<PerspectiveChangeEvent>
+{
+    public GameObject[] TopDownGameObjects;
+    public float DelayBeforeDeactivation = 2;
+    private Perspectives _perspective;
+    private float _deactivationTime;
+
+    private void Awake()
+    {
+        this.MMEventStartListening();
+        enabled = false;
+    }
+
+    private void OnDestroy()
+    {
+        this.MMEventStopListening();
+    }
+    
+    public void OnMMEvent(PerspectiveChangeEvent perspectiveChangeEvent)
+    {
+        var newPerspective = perspectiveChangeEvent.NewPerspective;
+        if (_perspective == newPerspective) return;
+        _perspective = newPerspective;
+        if (_perspective == Perspectives.FirstPerson)
+        {
+            _deactivationTime = Time.time + DelayBeforeDeactivation;
+            enabled = true;
+        }
+        else
+        {
+            foreach (var gameObjectToDeactivate in TopDownGameObjects)
+                gameObjectToDeactivate.SetActive(true);
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Time.time < _deactivationTime) return;
+        foreach (var gameObjectToDeactivate in TopDownGameObjects)
+            gameObjectToDeactivate.SetActive(false);
+        enabled = false;
+    }
+}
