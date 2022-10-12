091a59bb1bfbabf94592b20dbdd4b8f58daa0e12
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-10-12 21:49:08 +0300

    ✨ Adds TankControls

---
+++ TankControls/CharacterRotation.cs
@@ -0,0 +1,15 @@
+﻿using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+namespace TankControls
+{
+    public class CharacterRotation : MonoBehaviour
+    {
+        public float Speed = 5;
+        private GameObject _model;
+
+        private void Awake() => _model = GetComponentInParent<Character>().CharacterModel;
+
+        private void Update() => _model.transform.Rotate(0.0f, Speed * Input.GetAxis("Horizontal"), 0.0f);
+    }
+}
