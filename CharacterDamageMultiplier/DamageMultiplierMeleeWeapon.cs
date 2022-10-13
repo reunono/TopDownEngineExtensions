5a7ca8b159f81cdfe6b03bf23bbfb56312aa54aa
Alexandru Gheorghe <alexandru.gheorghe@my.fmi.unibuc.ro>
2022-10-13 14:01:39 +0300

    ❌ Deletes obsolete CharacterDamageMultiplier extension

--- CharacterDamageMultiplier/DamageMultiplierMeleeWeapon.cs
+++
@@ -1,9 +0,0 @@
-﻿using MoreMountains.TopDownEngine;
-
-namespace TopDownEngineExtensions
-{
-    public class DamageMultiplierMeleeWeapon : MeleeWeapon
-    {
-        public override void ApplyDamageMultiplier(float multiplier) { _damageOnTouch.DamageCaused = (int)(DamageCaused * multiplier); }
-    }
-}
